5e019f92c1bf18207564de139e5c71cf81a747f8
aksh10patil <rajdeeppatil2021@gmail.com>
2026-03-09 16:44:23 +0530

    Added the update for text

--- public/components/ui/Hero.tsx
+++ public/components/ui/Hero.tsx
@@ -236,12 +236,12 @@ export default function HeroSection() {
                 </div>
 
                 {/* Bottom Left Title */}
-                <div className="mt-120 md:mb-8">
+                <div className="mt-auto mb-[4vh]">
                     <motion.h1
                         variants={luxuryTypewriterContainer}
                         initial="hidden"
                         animate="visible"
-                        className="text-[clamp(90px,10.4vw,300px)] tracking-[-0.02em] font-light flex items-center whitespace-nowrap text-[#d6cdb7] translate-x-[1vw]"
+                        className="text-[clamp(90px,10.4vw,300px)] leading-none tracking-[-0.02em] font-light flex items-center whitespace-nowrap text-[#d6cdb7] translate-x-[1vw]"
                     >
                         {titleText.split("").map((char, index) => (
                             <motion.span key={index} variants={luxuryLetter}>
